e49c864b6ae638ba510846cfbf5251dd53d8da2a
tyrekosi <elkhouriela@gmail.com>
2023-09-12 20:00:10 -0400

    Changed to gif -> webp -> gif method instead of pure frame skipping.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # cli-gif-compressor
-Simple Python CLI tool to compress gifs by frame skipping.
+Simple Python CLI tool to compress gifs by converting them to WEBPs
 
-Slightly improve version of one of these I made for a personal project. You might have a seizure I'm ngl it goes fast.
+Slightly improve version of one of these I made for a personal project.
 
-Will update whenever I feel like making a lossless ver.
+# Usage
+usage: `cli-gif-compressor.py [-h] [-m SHRINKAGE_MODIFIER] [-r FRAME_RESIZE] [-s SKIP_FRAMES] input_path output_path`
+
+Losslessly compress a GIF using WebP.
+
+```positional arguments:
+  input_path            Path to the input GIF file.
+  output_path           Path where the compressed GIF will be saved.
+
+options:
+  -h, --help            show this help message and exit
+  -m SHRINKAGE_MODIFIER, --shrinkage_modifier SHRINKAGE_MODIFIER
+                        Factor by which you want to shrink the file size. E.g., 1.2 means you want the output file to be at most 1/1.2 of the original. (default: 1.2)
+  -r FRAME_RESIZE, --frame_resize FRAME_RESIZE
+                        Factor to resize each frame. Values should be between 0 and 1. (default: 0.8)
+  -s SKIP_FRAMES, --skip_frames SKIP_FRAMES
+                        Skip frames during reading to reduce the frame count. Value should be greater than 0. (default: 1)
+```
--- cli-gif-compressor.py
+++ cli-gif-compressor.py
@@ -1,52 +1,72 @@
 import argparse
-from PIL import Image, UnidentifiedImageError
 import os
+from PIL import Image, UnidentifiedImageError
+import imageio
 
-def reduce_frame_count(input_path, output_path, skip_frames=1):
+def compress_gif(input_path, output_path, shrinkage_modifier, frame_resize=0.8, skip_frames=1):
+    # Check if input file exists
+    if not os.path.exists(input_path):
+        print(f"Error: The specified input file '{input_path}' does not exist.")
+        return
+
+    # Check original file size
     try:
-        image = Image.open(input_path)
-    except FileNotFoundError:
-        print(f"Error: File '{input_path}' not found.")
-        return
-    except UnidentifiedImageError:
-        print(f"Error: '{input_path}' is not a valid image file.")
-        return
-    
-    if not image.format == 'GIF':
-        print(f"Error: '{input_path}' is not a GIF.")
-        return
-
-    if skip_frames < 1:
-        print("Error: Number of frames to skip must be at least 1.")
-        return
-
-    frames = []
-    frame_count = 0
-    
-    try:
-        while True:
-            if frame_count % skip_frames == 0:
-                frames.append(image.copy())
-            
-            image.seek(image.tell() + 1)
-            frame_count += 1
-    except EOFError:
-        pass
-
-    try:
-        frames[0].save(output_path, save_all=True, append_images=frames[1:], loop=0)
+        original_size = os.path.getsize(input_path)
     except Exception as e:
-        print(f"Error: Could not save the output file. {e}")
-    
-    print("Operation completed successfully.")
+        print(f"Error: Could not get the file size of '{input_path}'. {e}")
+        return
+
+    temp_webp_path = "temp.webp"
+    temp_gif_path = "temp.gif"
+
+    # Preprocess by resizing and reducing frame count
+    try:
+        with imageio.get_reader(input_path) as reader:
+            frames = [Image.fromarray(frame) for i, frame in enumerate(reader) if i % skip_frames == 0]
+            if frame_resize != 1.0:
+                frames = [frame.resize([int(dim * frame_resize) for dim in frame.size]) for frame in frames]
+            imageio.mimsave(temp_gif_path, [frame for frame in frames], format="gif")
+    except Exception as e:
+        print(f"Error during preprocessing: {e}")
+        return
+
+    input_path = temp_gif_path  # New input path is the resized/reduced GIF
+
+    while True:
+        try:
+            # Convert GIF to WebP
+            imageio.mimwrite(temp_webp_path, imageio.mimread(input_path, memtest=False), format="webp")
+
+            # Convert WebP back to GIF
+            imageio.mimwrite(temp_gif_path, imageio.mimread(temp_webp_path, memtest=False), format="gif")
+        except Exception as e:
+            print(f"Error during conversion: {e}")
+            return
+
+        compressed_size = os.path.getsize(temp_gif_path)
+
+        # Check if desired shrinkage is reached
+        if original_size / compressed_size >= shrinkage_modifier:
+            os.rename(temp_gif_path, output_path)
+            os.remove(temp_webp_path)
+            print("Operation completed successfully.")
+            return
+        else:
+            input_path = temp_gif_path  # Set input for next iteration to the compressed GIF
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Compress a GIF by reducing the frame count.")
+    parser = argparse.ArgumentParser(description="Losslessly compress a GIF using WebP.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
-    parser.add_argument("input_path", type=str, help="Path to the input GIF")
-    parser.add_argument("output_path", type=str, help="Path to save the output GIF")
-    parser.add_argument("-s", "--skip_frames", type=int, default=1, help="Number of frames to skip")
+    parser.add_argument("input_path", type=str, help="Path to the input GIF file.")
+    parser.add_argument("output_path", type=str, help="Path where the compressed GIF will be saved.")
+    parser.add_argument("-m", "--shrinkage_modifier", type=float, default=1.2, 
+                        help="Factor by which you want to shrink the file size. E.g., 1.2 means you want the output file to be at most 1/1.2 of the original.")
+    parser.add_argument("-r", "--frame_resize", type=float, default=0.8, 
+                        help="Factor to resize each frame. Values should be between 0 and 1.")
+    parser.add_argument("-s", "--skip_frames", type=int, default=1, 
+                        help="Skip frames during reading to reduce the frame count. Value should be greater than 0.")
     
     args = parser.parse_args()
     
-    reduce_frame_count(args.input_path, args.output_path, args.skip_frames)
+    compress_gif(args.input_path, args.output_path, args.shrinkage_modifier, args.frame_resize, args.skip_frames)
